353e48854c859d60fac37ef3dc79b26e0547847f
Lukas Blaha <lukas.blaha@tietoevry.com>
2024-10-20 12:23:19 +0200

    Update backend to send notifications

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 )
 
+func sendNotification(w http.ResponseWriter, r *http.Request) {
+	var msg string
+	url := "https://ntfy.sh/lukebl-workout"
+
+	b, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Println(err)
+	}
+
+	msg = string(b)
+
+	resp, err := http.Post(url, "text/plain", bytes.NewBuffer([]byte(msg)))
+	if err != nil {
+		log.Println(err)
+	}
+
+	fmt.Fprintf(w, fmt.Sprintf("%d\n", resp.StatusCode))
+}
+
 func main() {
 	router := http.NewServeMux()
 
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "hello world")
-	})
+	router.HandleFunc("POST /notify", sendNotification)
 
 	if err := http.ListenAndServe(":8080", router); err != nil {
 		log.Println(err)
